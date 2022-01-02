9e27f30fd9eddf62cebf0f50f349490f5fd988a0
idenet <leimiliya@hotmail.com>
2022-01-02 21:56:31 +0800

    Deploying to gh-pages from @ idenet/blog@676cc971180a18205bbd7de9cfd2fa52fbcfadcd 🚀

---
+++ assets/createElement.html.9a9cfcae.js
@@ -0,0 +1,1 @@
+const e={key:"v-2c8a1820",path:"/vue/render/createElement.html",title:"createElement",lang:"zh-CN",frontmatter:{},excerpt:"",headers:[{level:2,title:"patchVnode --> diff\u8FC7\u7A0B",slug:"patchvnode-diff\u8FC7\u7A0B",children:[]}],git:{updatedTime:1641131697e3},filePathRelative:"vue/render/createElement.md"};export{e as data};
