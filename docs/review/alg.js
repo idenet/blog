e829acfa916bdcaf1196fbdc05ce7df002ba76b2
香香鸡 <leimiliya@hotmail.com>
2022-02-08 21:03:56 +0800

    feat: 了解webpack

--- docs/review/alg.js
+++ docs/review/alg.js
@@ -1,4 +1,44 @@
+
+// 二叉树
 function treeNode (val) {
   this.val = val
   this.left = this.right = null
 }
+
+
+
+// 二叉树
+const root = {
+  val: "A",
+  left: {
+    val: "B",
+    left: {
+      val: "D"
+    },
+    right: {
+      val: "E"
+    }
+  },
+  right: {
+    val: "C",
+    right: {
+      val: "F"
+    }
+  }
+}
+
+// 先序遍历
+
+function preorder (root) {
+  // 递归边界
+
+  if (!root) return
+
+  console.log('输出当前遍历的节点值', root.val)
+  // 递归左子树
+  preorder(root.left)
+  // 递归右子树
+  preorder(root.right)
+}
+
+preorder(root)
