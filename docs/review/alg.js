4f31216463733f8ea50ba1ac725942d2449aee12
香香鸡 <leimiliya@hotmail.com>
2022-02-20 19:28:44 +0800

    feat: 翻转链表

--- docs/review/alg.js
+++ docs/review/alg.js
@@ -25,3 +25,35 @@ const longestCommonPrefix = function (strs) {
   }
   return result
 }
+
+/**
+ * 反转链表
+ */
+// 递归
+const reverseList = function (head) {
+  // 边界
+  if (!head || !head.next) return head
+  let last = reverseList(head.next)
+  head.next.next = head
+  head.next = null
+  return last
+}
+
+// 迭代
+
+const reverseList = function (head) {
+  // 初始节点 前驱节点为null
+  let pre = null
+  // 初始节点为头结点
+  let cur = head
+  while (cur !== null) {
+    // 记录下一个节点
+    let next = cur.next
+    // 反转指针
+    cur.next = pre
+    // 前进一步
+    pre = cur
+    cur = next
+  }
+  return pre
+}
